c71815ddcce64cc5f995064a508447b47f984e84
Trover <74741960+Troversones@users.noreply.github.com>
2021-09-21 10:42:24 +0200

    09.20 sütős szar
    kókány szar az egész

---
+++ 09.20/09.20/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace _09._20
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 401);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 37);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(374, 292);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(353, 158);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(146, 106);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kenyér",
+            "Zsömle",
+            "Kalács",
+            "Csirke",
+            "Rakott krumpli"});
+            this.comboBox1.Location = new System.Drawing.Point(43, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Válassz egy terméket";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Sütés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(248, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kész";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(335, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "x";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 201);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(505, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            this.label4.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 09.20/09.20/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09._20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image xd = new Bitmap(@"Z:\13.c\c#\0robux.jpg");
+            this.BackgroundImage = xd;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image lit = new Bitmap(@"Z:\13.c\c#\L I T.png");
+            pictureBox1.Image = lit;
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Interval = 1000;
+            progressBar1.Maximum = 30;
+            timer1.Tick += new EventHandler(timer1_Tick);
+            DateTime xdd = DateTime.Now.AddSeconds(progressBar1.Maximum);
+            string kek = xdd.ToString("T");
+            label4.Visible = true;
+            label4.Text = "Ekkor lesz kész: " + kek;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Kenyér")
+            {
+                Image kenyer = new Bitmap(@"Z:\13.c\c#\breadd.jpg");
+                pictureBox2.Image = kenyer;
+                label3.Text = "Kenyér";
+            }
+            else if(comboBox1.SelectedItem.ToString() == "Zsömle")
+            {
+                Image zsomle = new Bitmap(@"Z:\13.c\c#\bund.jfif");
+                pictureBox2.Image = zsomle;
+                label3.Text = "Zsemle";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Kalács")
+            {
+                Image kalacs = new Bitmap(@"Z:\13.c\c#\fonottkalacsd.jpg");
+                pictureBox2.Image = kalacs;
+                label3.Text = "Fonott kalács";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Csirke")
+            {
+                Image csirke = new Bitmap(@"Z:\13.c\c#\csirked.jpg");
+                pictureBox2.Image = csirke;
+                label3.Text = "Csirke";
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Rakott krumpli")
+            {
+                Image krumpli = new Bitmap(@"Z:\13.c\c#\rakott.jpg");
+                pictureBox2.Image = krumpli;
+                label3.Text = "Rakott krumpli";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value != 30)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                label2.Visible = true;
+                timer1.Stop();
+                if (comboBox1.SelectedItem.ToString() == "Kenyér")
+                {
+                    Image kenyer = new Bitmap(@"Z:\13.c\c#\kenyerkesz.jpg");
+                    pictureBox2.Image = kenyer;
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Zsömle")
+                {
+                    Image zsomle = new Bitmap(@"Z:\13.c\c#\zsemlekesz.jfif");
+                    pictureBox2.Image = zsomle;
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Kalács")
+                {
+                    Image kalacs = new Bitmap(@"Z:\13.c\c#\fonottkesz.jpg");
+                    pictureBox2.Image = kalacs;
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Csirke")
+                {
+                    Image csirke = new Bitmap(@"Z:\13.c\c#\csirkekesz.jpg");
+                    pictureBox2.Image = csirke;
+                }
+                else if (comboBox1.SelectedItem.ToString() == "Rakott krumpli")
+                {
+                    Image krumpli = new Bitmap(@"Z:\13.c\c#\rakottkesz.jpeg");
+                    pictureBox2.Image = krumpli;
+                }
+                MessageBox.Show("Kész van a kiválaszott ételed \r\n Ez van tedd zsebre.");
+                this.Close();
+            }
+
+            
+        }
+    }
+}
